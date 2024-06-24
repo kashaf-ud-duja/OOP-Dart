2e767d92fc9b9ee08e75e1c49fd01f3ffd8cfdea
kashaf-ud-duja <tahirkashaf7@gmail.com>
2024-06-24 15:03:03 +0500

    dart null safety concept

---
+++ main.dart
@@ -0,0 +1,17 @@
+void main(){
+  int? age;
+  String? name;
+
+
+
+ print(name);
+//  we can handle null variable in dart by using conditions i.e. "if condition"
+ if(name== null){
+print("name is null");
+ }
+  print(age);
+  age=10;
+  name= "Kashaf";
+  print(age);
+   print(name);
+}
