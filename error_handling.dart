1820024bfb5e86c0999cb8388ad9a9d2c4f7b334
kashaf-ud-duja <tahirkashaf7@gmail.com>
2024-07-04 17:18:49 +0500

    Dart exception or error handling

--- error_handling.dart
+++ error_handling.dart
@@ -1,12 +1,12 @@
 void main() {
 Area area = Area();
-area.length = 14;
+area.height = 14;
 area.calculateArea();
 
 }
 class Area{
-  double? length;
-  double? width;
+  double height=0;
+  double width=0;
 
   void calculateArea(){
     print(width);
@@ -14,11 +14,8 @@ class Area{
   // print('Area f rectangle is ${rectangleArea}');
   // try and catch value is used for exception handling(here e stands for exception)
   try {
-    if (width== null) {
-      print("width cannot be null");
-    }
-    double rectangleArea = length!*width!;
-  print('Area f rectangle is ${rectangleArea}');
+    double rectangleArea = height*width;
+  print('Area of rectangle is ${rectangleArea}');
   } catch (e) {
     print(e);
   }
