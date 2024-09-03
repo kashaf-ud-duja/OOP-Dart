f0f4c7ce93c78bbddbbade39f18dc3094d031571
kashaf-ud-duja <tahirkashaf7@gmail.com>
2024-09-03 12:29:22 +0500

    Constructor inheritance

--- inheritance_of_constructors.dart
+++ inheritance_of_constructors.dart
@@ -1,24 +1,31 @@
 import 'dart:ffi';
 
 void main(){
-MacBook macBook = MacBook('Macmini', 20000);
-macBook.displayInfo();
+MacBook macBook = MacBook('Macmini', 20000, '14');
+macBook.displayDetails();
 }
-//Parent class or Super class
+//Parent class or Super class or base class
 class Leptop{
   String brand;
   double price;
+  //we will inherit this parameters that we have passed in the constructor,that is called inheritance through constructor
 Leptop(this.brand,this.price){
-  print('Constructor get called');
+  print('leptop called');
 }
 
 
 void displayInfo(){
-print('the leptop brand is ${brand} and the price is ${price}');
+print('brand: ${brand}, price: ${price}');
 }
 
 }
-
+//Sub class , child class
 class MacBook  extends Leptop{
- MacBook(String brand,double price ) : super(brand ,price);
+String model;
+//when we inherit a class who has a consturctor , then you must have to inherit constructor in such a way that , consturctor of child class : super keyword and constructor parameters i.e. constructorchild() : super ("mac mini", 240000)
+ MacBook(String brand,double price, this.model ) : super(brand ,price);
+ void displayDetails(){
+  super.displayInfo();
+print('model: ${model}');
+}
 }
